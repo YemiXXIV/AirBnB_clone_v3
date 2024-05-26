1539cc10ebc7c3204161c2c27aa1cf65873ba610
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-26 22:10:42 +0300

    Updating models/engine/file_storage.py and models/engine/db_storage.py files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,9 @@
 Contains the class DBStorage
 """
 
-import models
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -11,10 +13,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from os import getenv
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
            'State': State, 'City': City, 'Amenity': Amenity,
