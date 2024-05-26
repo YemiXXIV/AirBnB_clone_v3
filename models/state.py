af64108e05333cc38264a1c0aa483cf0ba8a586f
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-26 22:39:59 +0300

    Updating models/state.py file

--- models/state.py
+++ models/state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ holds class State"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -17,10 +15,6 @@ class State(BaseModel, Base):
         cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
 
     if models.storage_t != "db":
         @property
