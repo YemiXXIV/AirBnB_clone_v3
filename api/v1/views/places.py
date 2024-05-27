9379641b16a6111576761fe62b567d6f042e132c
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-28 02:53:30 +0300

    Updating api/v1/views/places.py file

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -116,7 +116,6 @@ def update_place(place_id):
     return jsonify(place.to_dict()), 200
 
 
-@app_views.route("places_search", methods=["POST"], strict_slashes=False)
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
 def post_places_search():
     """searches for a place"""
@@ -127,21 +126,21 @@ def post_places_search():
         amenities = params.get('amenities', [])
         amenity_objects = []
         for amenity_id in amenities:
-            amenity = storage.get('Amenity', amenity_id)
+            amenity = storage.get(Amenity, amenity_id)
             if amenity:
                 amenity_objects.append(amenity)
         if states == cities == []:
-            places = storage.all('Place').values()
+            places = storage.all(Place).values()
         else:
             places = []
             for state_id in states:
-                state = storage.get('State', state_id)
+                state = storage.get(State, state_id)
                 state_cities = state.cities
                 for city in state_cities:
                     if city.id not in cities:
                         cities.append(city.id)
             for city_id in cities:
-                city = storage.get('City', city_id)
+                city = storage.get(City, city_id)
                 for place in city.places:
                     places.append(place)
         confirmed_places = []
